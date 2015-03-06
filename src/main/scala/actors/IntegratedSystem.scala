f9c818ffd9ba247f819e7f45d4723b0fe760d1af
Jentsch <d.jentsch@fu-berlin.de>
2015-03-06 18:11:50 +0100

    Added a draft for integrated systems with external dependencies

---
+++ src/main/scala/actors/IntegratedSystem.scala
@@ -0,0 +1,18 @@
+package actors
+
+/**
+ * *Contains snippets and ideas*
+ *
+ * Allows the usage of external systems like web services.
+ */
+trait IntegratedSystem extends ActorSystem {
+  trait ExternalService extends Actor {
+    val init: Behaviour = { case _ => ??? }
+  }
+
+  class WebService extends ExternalService
+
+  class JsonWebService extends WebService
+
+  class UdpService extends ExternalService
+}

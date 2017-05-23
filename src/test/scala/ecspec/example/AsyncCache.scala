c55224a8407dddce768c533af7623204fbe3a38d
Jentsch <d@jentsch.berlin>
2017-05-23 17:49:56 +0200

    Made example more presentation friendly

--- src/test/scala/ecspec/example/AsyncCache.scala
+++ src/test/scala/ecspec/example/AsyncCache.scala
@@ -4,31 +4,23 @@ import scala.collection.concurrent.TrieMap
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success}
 
-class AsyncCache[Request, Response](f: Request => Future[Response])(
-    implicit ec: ExecutionContext)
-    extends (Request => Future[Response]) {
-
-  private val store = TrieMap.empty[Request, Promise[Response]]
-
-  override def apply(request: Request): Future[Response] = {
-    val myPromise = Promise[Response]
-    val response = store.getOrElseUpdate(request, myPromise)
-
-    if (response eq myPromise) {
-      f(request).onComplete {
-        case Success(s) => myPromise.success(s)
-        case Failure(f) =>
-          store.remove(request)
-          myPromise.failure(f)
-      }
-    }
-
-    response.future
-  }
-}
-
 object AsyncCache {
-  def apply[Request, Response](f: Request => Future[Response])(
-      implicit ec: ExecutionContext): AsyncCache[Request, Response] =
-    new AsyncCache(f)
+  def apply[In, Out](f: In => Future[Out])(
+      implicit ec: ExecutionContext): In => Future[Out] = {
+    val store = TrieMap.empty[In, Promise[Out]]
+
+    { key =>
+      val myPromise = Promise[Out]
+      val res = store.getOrElseUpdate(key, myPromise)
+      if (res eq myPromise)
+        f(key).onComplete {
+          case Success(s) => myPromise.success(s)
+          case Failure(f) =>
+            store.remove(key)
+            myPromise.failure(f)
+        }
+
+      res.future
+    }
+  }
 }
--- src/test/scala/ecspec/example/AsyncCacheSpec.scala
+++ src/test/scala/ecspec/example/AsyncCacheSpec.scala
@@ -3,60 +3,68 @@ package ecspec.example
 import ecspec.EcSpec
 import org.scalatest.{FlatSpec, Matchers}
 
-import scala.collection.mutable.ListBuffer
 import scala.concurrent.Future
-import scala.concurrent.Future.successful
 import scala.util.{Failure, Success, Try}
 
-class AsyncCacheSpec extends FlatSpec with EcSpec with Matchers {
+class AsyncCacheSpec
+    extends FlatSpec
+    with EcSpec
+    with Matchers {
 
-  "Async Cache" should "return same result as given function" in everyInterleaving {
+  behavior of "Async Cache"
+
+  it should "return same result" in everyInterleaving {
     implicit ec =>
-      val f = Map(1 -> successful("1"))
-      val cache = AsyncCache(f)
-
-      ec.execute { () =>
-        cache(1) will be("1")
-      }
-      ec.execute { () =>
-        cache(1) will be("1")
-      }
-  }
-
-  it should "not invoke the function twice for same argument" in everyInterleaving {
-    implicit ec =>
-      val observedRequests = ListBuffer.empty[Int]
       val f = { i: Int =>
-        Future {
-          observedRequests += i
-          i.toString
-        }
+        Future { i.toString }
       }
       val cache = AsyncCache(f)
 
-      ec.execute { () =>
-        cache(1)
-      }
-      ec.execute { () =>
-        cache(1)
-      }
-
-      observedRequests.diff(List(1, 2)) shouldBe empty
+      cache(1) will be("1")
+      cache(1) will be("1")
   }
 
-  it should "not store failures" in everyInterleaving { implicit ec =>
-    var response: Try[String] = Failure[String](new RuntimeException(""))
-    val f = { _: Int =>
-      Future { response.get }
+  it should "cache" in everyInterleaving { implicit ec =>
+    var seenRequest = Set.empty[Int]
+
+    val f = { i: Int =>
+      seenRequest should not contain i
+      seenRequest += i
+      Future {
+        i.toString
+      }
     }
     val cache = AsyncCache(f)
 
-    cache(1).onComplete { t =>
-      t should be a 'failure
-
-      response = Success("1")
-
-      cache(1) will be("1")
+    atomic {
+      ec.execute(() => cache(1) will be("1"))
+      ec.execute(() => cache(1) will be("1"))
     }
+  }
+
+  it should "not store failures" in everyInterleaving {
+    implicit ec =>
+      var response: Try[String] =
+        Failure(new RuntimeException(""))
+
+      val f = { _: Int =>
+        Future { response.get }
+      }
+
+      val cache = AsyncCache(f)
+
+      atomic {
+        cache(1).onComplete { r =>
+          r should be a 'failure
+          response = Success("What ever")
+
+          cache(1) will be("What ever")
+        }
+
+        cache(1).onComplete { respone =>
+          response could be(a[Failure[_]])
+          response could be(a[Success[_]])
+        }
+      }
   }
 }

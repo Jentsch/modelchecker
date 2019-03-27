d7008f29337beba49d52ad381be8a8d79c2a89aa
Jentsch <d@jentsch.berlin>
2019-03-27 16:17:59 +0100

    Compact scalaz philosophers

--- scalaz/src/test/scala/berlin/jentsch/modelchecker/scalaz/example/Philosophers.scala
+++ scalaz/src/test/scala/berlin/jentsch/modelchecker/scalaz/example/Philosophers.scala
@@ -2,36 +2,37 @@ package berlin.jentsch.modelchecker.scalaz.example
 
 import berlin.jentsch.modelchecker.scalaz.Interpreter
 import org.scalatest.{FlatSpec, Matchers}
-import scalaz.zio._
+import scalaz.zio.{UIO, Semaphore}
+import scalaz.zio.UIO._
 
 object Philosophers {
 
   private def philosopher(firstStick: Semaphore,
                           secondStick: Semaphore): UIO[Unit] =
-    IO.succeed((/* THINK */ )) *>
+    succeed((/* THINK */ )) *>
       firstStick.acquire *>
       secondStick.acquire *>
-      IO.succeed((/* EAT */ )) *>
+      succeed((/* EAT */ )) *>
       secondStick.release *>
       firstStick.release
 
   def run(n: Int): UIO[Unit] =
     for {
-      sticks <- IO.foreach(in = 0 until n)(_ => Semaphore.make(1))
+      sticks <- foreach(in = 0 until n)(_ => Semaphore.make(1))
       philosophers = 0 until n map { i =>
         philosopher(sticks(i), sticks((i + 1) % n))
       }
-      _ <- IO.foreachPar(philosophers)(identity)
+      _ <- collectAllPar(philosophers)
     } yield ()
 
   def runOk(n: Int): UIO[Unit] =
     for {
-      sticks <- IO.foreach(in = 0 until n)(_ => Semaphore.make(1))
+      sticks <- foreach(in = 0 until n)(_ => Semaphore.make(1))
       philosophers = 0 until n map {
         case 0 => philosopher(sticks(n - 1), sticks(0))
         case i => philosopher(sticks(i), sticks((i + 1) % n))
       }
-      _ <- IO.foreachPar(philosophers)(identity)
+      _ <- collectAllPar(philosophers)
     } yield ()
 }
 

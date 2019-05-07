e6402d20de5ca9eae8cb7eb8c2f772ab796de2e8
Jentsch <d@jentsch.berlin>
2019-05-08 00:42:42 +0200

    Improve scalaz Philosophers

--- scalaz/src/test/scala/berlin/jentsch/modelchecker/scalaz/example/Philosophers.scala
+++ scalaz/src/test/scala/berlin/jentsch/modelchecker/scalaz/example/Philosophers.scala
@@ -1,6 +1,6 @@
 package berlin.jentsch.modelchecker.scalaz.example
 
-import berlin.jentsch.modelchecker.scalaz.Interpreter
+import berlin.jentsch.modelchecker.scalaz.Interpreter._
 import org.scalatest.{FlatSpec, Matchers}
 import scalaz.zio.UIO._
 import scalaz.zio.{Semaphore, UIO}
@@ -21,20 +21,18 @@ object Philosophers {
   def run(n: Int): UIO[Unit] =
     for {
       sticks <- foreach(in = 0 until n)(_ => Semaphore.make(1))
-      philosophers = 0 until n map { i =>
+      _ <- foreachPar(0 until n) { i =>
         philosopher(sticks(i), sticks((i + 1) % n))
       }
-      _ <- collectAllPar(philosophers)
     } yield ()
 
   def runOk(n: Int): UIO[Unit] =
     for {
       sticks <- foreach(in = 0 until n)(_ => Semaphore.make(1))
-      philosophers = 0 until n map {
+      _ <- foreachPar(0 until n) {
         case 0 => philosopher(sticks(n - 1), sticks(0))
         case i => philosopher(sticks(i), sticks((i + 1) % n))
       }
-      _ <- collectAllPar(philosophers)
     } yield ()
 }
 
@@ -42,12 +40,10 @@ class PhilosophersSpec extends FlatSpec with Matchers {
   behavior of "Philosophers"
 
   they should "sometimes deadlock in wrong configuration" in {
-    Interpreter.notFailing(Philosophers.run(3)) should (contain(None) and contain(
-      Some(())
-    ))
+    all(notFailing(Philosophers.run(3))) should (be(None) or be(Some(())))
   }
 
   they should "never deadlock" in {
-    all(Interpreter.notFailing(Philosophers.runOk(3))) should be(Some(()))
+    all(notFailing(Philosophers.runOk(3))) should be(Some(()))
   }
 }

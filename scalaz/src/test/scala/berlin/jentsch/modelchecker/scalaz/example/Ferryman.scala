55586964ea218748119698c7826d8299f83fb3f1
Jentsch <d@jentsch.berlin>
2019-05-09 10:15:32 +0200

    Clean up scalaz ferryman

--- scalaz/src/test/scala/berlin/jentsch/modelchecker/scalaz/example/Ferryman.scala
+++ scalaz/src/test/scala/berlin/jentsch/modelchecker/scalaz/example/Ferryman.scala
@@ -6,54 +6,54 @@ import berlin.jentsch.modelchecker.scalaz.NonDeterministic.doOneOf
 import org.scalatest.matchers.Matcher
 import org.scalatest.{Assertion, FlatSpec, Matchers}
 import scalaz.zio.Exit.Success
+import scalaz.zio._
 import scalaz.zio.syntax._
-import scalaz.zio.{Exit, _}
 
 object Ferryman {
 
   type Error = Unit
   val Error: Error = ()
 
-  def randomM: ZIO[NonDeterministic, Error, Unit] =
+  def ferryman: ZIO[NonDeterministic, Error, Unit] =
     for {
       wolf <- Ref.make(false)
       goat <- Ref.make(false)
       cabbage <- Ref.make(false)
       ferryMan <- Ref.make(false)
       limit <- Ref.make(8)
-      _ <- repeatUntil(wolf.get && goat.get && cabbage.get && ferryMan.get)(
-        doOneOf(
-          sameSide(wolf, ferryMan) -> carry(wolf) *> carry(ferryMan),
-          sameSide(goat, ferryMan) -> carry(goat) *> carry(ferryMan),
-          sameSide(cabbage, ferryMan) -> carry(cabbage) *> carry(ferryMan),
+      _ <- repeatUntil(allCarriedOver(wolf, goat, cabbage, ferryMan))(
+        doOneOf( // rules
+          (wolf.get === ferryMan.get) -> carry(wolf) *> carry(ferryMan),
+          (goat.get === ferryMan.get) -> carry(goat) *> carry(ferryMan),
+          (cabbage.get === ferryMan.get) -> carry(cabbage) *> carry(ferryMan),
           true.succeed -> carry(ferryMan)
-        ) *>
+        ) *> // Check for invalid states
           check(wolf.get =!= goat.get || wolf.get === ferryMan.get) *>
           check(goat.get =!= cabbage.get || goat.get === ferryMan.get) *>
           check(limit.update(_ - 1).map(_ > 0))
       )
     } yield ()
 
+  private def allCarriedOver(items: Ref[Boolean]*): UIO[Boolean] =
+    items.foldLeft(true.succeed)((f, r) => f.zipWith(r.get)(_ && _))
+
   /** Lifts some some boolean operations into UIO */
   implicit class UioBooleanSyntax(zio: UIO[Boolean]) {
-    def &&(other: UIO[Boolean]): ZIO[Any, Nothing, Boolean] =
+    def &&(other: UIO[Boolean]): UIO[Boolean] =
       zio.zipWith(other)(_ && _)
 
-    def ||(other: UIO[Boolean]): ZIO[Any, Nothing, Boolean] =
+    def ||(other: UIO[Boolean]): UIO[Boolean] =
       zio.zipWith(other)(_ || _)
 
-    def ===(other: UIO[Boolean]): ZIO[Any, Nothing, Boolean] =
+    def ===(other: UIO[Boolean]): UIO[Boolean] =
       zio.zipWith(other)(_ == _)
 
-    def =!=(other: UIO[Boolean]): ZIO[Any, Nothing, Boolean] =
+    def =!=(other: UIO[Boolean]): UIO[Boolean] =
       zio.zipWith(other)(_ != _)
   }
 
   def carry(ref: Ref[Boolean]): ZIO[Any, Nothing, Unit] =
     ref.update(!_).void
-
-  def sameSide(item1: Ref[Boolean], item2: Ref[Boolean]): UIO[Boolean] =
-    item1.get.zipWith(item2.get)(_ == _)
 
   def repeatUntil[R, E](
       condition: UIO[Boolean]
@@ -64,19 +64,19 @@ object Ferryman {
     }
 
   def check(predicate: UIO[Boolean]): IO[Error, Unit] =
-    IO.whenM(predicate.map(!_))(ZIO.fail(Error))
+    IO.whenM(predicate.map(!_))(IO.fail(Error))
 }
 
 class FerrymanSpec extends FlatSpec with Matchers {
   behavior of "ferryman"
 
-  implicit class Syntax[E, A](results: Set[Exit[E, A]]) {
-    def could(matcher: Matcher[Any]): Assertion =
+  implicit class Syntax[A](results: Set[A]) {
+    def could(matcher: Matcher[A]): Assertion =
       atLeast(1, results) should matcher
   }
 
   it should "be possible to carry over everything" in {
-    everyPath.terminates(Ferryman.randomM) could be(a[Success[_]])
+    everyPath.terminates(Ferryman.ferryman) could be(a[Success[_]])
   }
 
 }
